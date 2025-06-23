3b11df4ebb59d2393d6e26526565d16ab9ece511
Ccikari <caiorocha0308@hotmail.com>
2025-06-22 22:53:18 -0300

    desafios aula 13

--- Curso Guanabara/Mundo 2/ex51_aula13.py
+++ Curso Guanabara/Mundo 2/ex51_aula13.py
@@ -42,13 +42,74 @@ else:
 
 # 53. Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços
     # Ex: Apos a sopa; a sacada da casa; a torre da derrota; o lobo ama o bolo; anotaram a data da maratona
+frase = str(input("Frase: ")).strip().upper()
+palavras = frase.split()
+junto = " ".join(palavras)
+inverso = " "
+for letra in range(len(junto) - 1, -1, -1): # dá pra fazer sem for, usando pal = f3[::-1]
+    inverso += junto[letra]
+print("O inverso de {} é {}".format(junto, inverso))
+if inverso == junto: 
+    print("É um palíndromo")
+else:
+    print("Não é um palíndromo") # por algum motivo não dá certo e não consigo descobrir o pq
 
 #54. Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas ainda não atingiram a maioridade e quantas já são maiores
     # maioridade: 21 anos
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for pessoa in range(1, 8):
+    nasc = int(input("Em que ano a {} pessoa nasceu? ".format(pessoa)))
+    idade = atual - nasc
+    if idade >= 21:
+        totmaior += 1
+    else: 
+        totmenor += 1
+print("São {} pessoas maiores de idade".format(totmaior))
+print("São {} pessoas menores de idade".format(totmenor))
 
 # 55. Faça um programa que leia o peso de cinco pessoa. No final, mostre qual foi o maior e o menor peso lidos
+maior = 0
+menor = 0
+for p in range(1, 6):
+    peso = float(input("O peso da {} pessoa: ".format(p)))
+    if p == 1:
+        maior = p
+        menor = p
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print("O maior peso foi {}Kg".format(maior))
+print("O menor peso foi {}Kg".format(menor))
 
 # 56. Desenvolva um programa que leia nome, idade e sexo de quatro pessoas. No final do programa, mostre:
     # A média de idade do grupo 
     # Qual é o nome do homem mais velho 
     # Quantas mulheres têm menos de 20 anos
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = " "
+totmulher20 = 0
+for pessoas in range(1, 5):
+    print("----- {} PESSOA -----".format(pessoas))
+    nome = str(input("Nome: ")).strip()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo [M/F]: ")).strip()
+    somaidade += idade
+    if pessoas == 1 and sexo in "Mm":
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in "Mm" and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in "Ff" and idade < 20:
+        totmulher20 += 1
+mediaidade = somaidade / 4
+print("A média de idade do grupo é de {} anos".format(mediaidade))
+print("O homem mais velho tem {} anos e se chama {}".format(maioridadehomem, nomevelho))
+print("São {} mulheres com menos de 20 anos".format(totmulher20))
